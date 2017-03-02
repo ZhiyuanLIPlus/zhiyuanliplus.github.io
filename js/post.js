5c29f0d31d44a38abe9164c011108c69a44a30dd
Zhiyuan LI <zhiyuan.liplus@gmail.com>
2017-03-02 11:36:09 +0100

    mix content issues

--- js/post.js
+++ js/post.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
     $('#disqus_container .comment').on('click',function(){
         $(this).html('加载中...');
         var that = this;
-        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
+        $.getScript('https://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
     });
 
     $('.entry a').each(function(index,element){
